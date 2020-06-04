f883eadf33afd4fc14a58798630715de61ae291c
alex <alexkonrad08@gmail.com>
2020-06-04 00:37:52 -0500

    Clean up

--- patchFeatures.m
+++ patchFeatures.m
@@ -10,17 +10,6 @@ function [xAbs,xRel] = patchFeatures(X, image)
 %    xAbs: absolute feature vectors
 %    xRel: relative feature vectors
 %
-
-% TODO: In section 3.1, do we compute absolute energy and sum of squared energy wiht
-% with just the intensity channel or all three channels of the image? Here
-% we just compute with the intensity channel.
-%
-% TODO: Add last three absolute features for column:
-% Lastly, many structures (such as trees and buildings) found in outdoor scenes 
-% show vertical structure, in the sense that they are vertically connected to 
-% themselves (things cannot hang in empty air). Thus, we also add to the 
-% features of a patch additional summary features of the column it lies in.
-
 Y = image(:,:,1);
 
 % Create WxHx34 feature vector computed for each patch to create
@@ -32,7 +21,6 @@ E = cat(3,YX,YX2);
 
 % Patch-level absolute feature matrix
 xAbs = zeros(H,W,D*19);
-% xAbs = double.empty(W,H,0);
 
 [f1,f2,f3,f4,f5] = neighborFilters(1);
 [f6,f7,f8,f9,f10] = neighborFilters(3);
@@ -45,44 +33,23 @@ F{3} = cat(3,f11,f12,f13,f14,f15);
 F{4} = cat(3,f16,f17,f18,f19);
 
 % Compute absolute feature values for this pixel
-% TODO remove this
-% xAbs(:,:,1:D) = E;
-
 % Rescale image to 3 different sizes, filter for pixel and 4 neighbors.
 % compute feature values for neighboring patches
-%   fprintf("%d %d %d\n", i, j, k);
-
-tic;
 for i=1:D:D*15
   [~,j,k] = ind2sub([D 5 3], i);
   xAbs(:,:,i:i+D-1) = convn(E,F{k}(:,:,j),'same');
-  fprintf("Feature rows %d-%d\n", i, i+D-1);
 end
-toc;
-tic;
+
+% Last four features are column features, which break up every column
+% into quadrants. This computes for each quadrant all features, so adds
+% 34x4=136 features to the vector.
 for i=D*15+1:D:D*19
   j = floor((i-510)/D)+1;
-%   xAbs(:,:,i:i+D-1) = repmat(F{4}(:,:,j)', H, 1);
-%   xAbs(:,:,i:i+D-1) = convn(E,F{4}(:,:,j),'same');
   for k=1:size(E,3)
     xAbs(:,:,i+k-1) = repmat(F{4}(:,:,j)'*E(:,:,k), H, 1);
-    fprintf("Feature row %d\n", i+k-1);
   end
 end
-toc;
-% end
-% for j=1:3
-%   for i=1:5
-%     idx = (sub2ind([5 3], i, j)-1)*D + 1; 
-% %     idx=i*j*D+1;
-%     xAbs(:,:,idx:idx+D-1) = convn(E,F{j}(:,:,i),'same');
-%   end
-% end
-
-
 
 xRel=zeros(1,1);
-% 
-% E1=convn(I, f, 'same');
 end
 
