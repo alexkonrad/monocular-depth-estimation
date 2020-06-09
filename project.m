32b86830aa5d14022ff44e6fc85e0f5a099235d9
Haitham Khedr <haithamkhedr@gmail.com>
2020-06-09 15:16:28 -0700

    Add params

--- project.m
+++ project.m
@@ -12,7 +12,7 @@ epochs = 50;
 %% Load training data
 thetaML = ones(646,1);    
 lambdaML = zeros(size(thetaML));
-for epoch = 1:epochs
+% for epoch = 1:epochs
     %Load same batch for now an see if we can overfit
     [trainX,trainY] = loadTrainingData(...
       imgDir,depthDir,batchSize,height,width,...
@@ -20,16 +20,8 @@ for epoch = 1:epochs
 
     [xAbs,xRel] = genFeatures(trainX, height, width);
     depths = upperScaleDepth(trainY);
+    X = reshape(xAbs,[],size(xAbs,4));
+    Y = reshape(depths(:,:,:,1),[],1);
     
-    %concatenate data
-    data = zeros(size(trainY,1),size(trainY,2),size(trainY,3),3 + size(xAbs,4));
-    data(:,:,:,1:size(xAbs,4)) = xAbs;
-    data(:,:,:,size(xAbs,4)+1:end) = depths;
-
+    thetaML = X \ Y;
     
-    funLL = @(theta)likelihood2(theta, data);
-
-
-    thetaML = L1General2_TMP(funLL, thetaML, lambdaML);
-%     [ll,grad] = likelihood(thetaML, data);
-end
